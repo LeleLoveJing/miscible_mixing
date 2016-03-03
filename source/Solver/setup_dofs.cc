8972eeec7f61a30d8f836f633548b42d91037ed7
jazz1950 <sungho.yoon7@gmail.com>
2016-03-03 21:36:44 +0900

    change_00

--- source/Solver/setup_dofs.cc
+++ source/Solver/setup_dofs.cc
@@ -273,10 +273,13 @@
 						}
     }
 
-    if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) ==
-								Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) - 1
-								&&
-							 parameters.domain_boundary[1] == 4
+//    if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) ==
+//								Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) - 1
+//								&&
+//							 parameters.domain_boundary[1] == 4
+//								&&
+//							 parameters.ist_pressure_boundary == 1)
+    if (parameters.domain_boundary[1] == 4
 								&&
 							 parameters.ist_pressure_boundary == 1)
     {
